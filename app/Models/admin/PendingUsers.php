24c2381c5a074f8bcad6b939189577077be79ad9
user.Vishal-devloper <vishal8901286897@gmail.com>
2025-09-24 15:58:18 +0530

    Pending Users Model added

---
+++ app/Models/admin/PendingUsers.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class PendingUsers extends Model
+{
+    protected $table = 'pending_users';
+    protected $primaryKey = 'payment_id';
+    protected $allowedFields = [ 
+     'name' ,'email', 'password', 'phone', 'store_name', 'verification_code', 'code_expires_at', 'created_at', 'status'	 
+    ];
+}
